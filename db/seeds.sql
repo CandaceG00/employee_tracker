a4afbbab8daf0f4d23f0041a4fbf00ffe87dea87
Candace Gostinski <candacegostinski@Candaces-MBP.lan>
2023-08-29 19:38:05 -0400

    added employee names to employees table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,11 @@
 USE employee_db;
+
+INSERT INTO employees(first_name, last_name, role_id, manager_id)
+VALUES 
+('Neil', 'Armstrong', 4, 0),
+('Edwin', 'Aldrin', 1, 0),
+('Michael', 'Collins', 57, 0),
+('James', 'Lovell', 35, 0),
+('Fred', 'Haise', 58, 3),
+('Jack', 'Swigert', 54, 0),
+('Pete', 'Conrad', 12, 0);
