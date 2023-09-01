6734b0c89f5a845aa4b6c2cf2df912c3d78f455c
Candace Gostinski <candacegostinski@Candaces-MBP.lan>
2023-09-01 11:36:49 -0400

    added db connection to server.js

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',
+    password: 'new_password',
+    database: 'employee_db'
+});
+
+connection.connect(function(err){
+    if (err) throw err;
+    cli_prompt();
+});
+
 
 const mainPrompt = [
     {
