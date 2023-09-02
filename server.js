66b0362395eaa9edeefec078238bc6b72314e731
Candace Gostinski <candacegostinski@Candaces-MBP.lan>
2023-09-02 10:52:16 -0400

    added updateemployee function to server.js

--- server.js
+++ server.js
@@ -395,3 +395,167 @@ function addRole() {
         });
     });
 };
+
+function updateEmployee() {
+    let query = "SELECT title FROM roles";    
+    let query2 =
+
+        "SELECT employees.first_name, employees.last_name, roles.title, roles.salary, department.dept_name, employees.manager_id " +
+        "FROM employees " +
+        "JOIN roles ON roles.id = employees.role_id " +
+        "JOIN department ON roles.department_id = department.id " +
+        "ORDER BY employees.id;"
+    ;
+
+    connection.query(query, function(err, res){
+        if (err) throw err;
+        let rolesList = res;
+        connection.query(query2, function(err,res) {            
+            if (err) throw err;
+            for(i = 0; i < res.length; i++) {
+                if(res[i].manager_id == 0) {
+                    res[i].manager = "None" 
+                }else{
+                    res[i].manager = res[res[i].manager_id - 1].first_name + " " + res[res[i].manager_id - 1].last_name;
+                };
+
+                delete res[i].manager_id;
+            };
+
+            console.table(res);
+
+            let employeeList = res;
+            let addEmpPrompt = [
+
+                {
+                    name: "select_employee",
+                    type: "list",
+                    message: "Select employee to edit",                    
+                    choices: function() {                        
+                        employees = [];            
+                        for(i = 0; i < employeeList.length; i++) {                            
+                            const mId = i + 1;
+                            employees.push(mId + ": " + employeeList[i].first_name + " " + employeeList[i].last_name);
+                        };
+                        
+                        employees.unshift("0: Exit");
+                        return employees;
+                    }
+                }
+            ];
+            
+            inquirer.prompt(addEmpPrompt)
+            .then(function(answer) {
+                if(answer.select_employee == "0: Exit") {
+                    cli_prompt();
+                }else{
+                    let empSelect = answer.select_employee.split(":")[0]
+                    let empPropPrompt = [
+                
+                        {
+                            name: "select_role",
+                            type: "list",
+                            message: "Edit employee role.",        
+                            choices: function() {                                
+                                roles = [];
+                                
+                                for(i = 0; i < rolesList.length; i++) {                                    
+                                    const roleId = i + 1;        
+                                    roles.push(roleId + ": " + rolesList[i].title);
+                                };
+
+                                roles.unshift("0: Exit");                                
+                                return roles;
+                            }
+                        },
+                        {
+                    
+                            name: "select_manager",
+                            type: "list",
+                            message: "Edit employee manager",
+                            choices: function() {                                
+                                managers = [];                    
+                                for(i = 0; i < employeeList.length; i++) {                                    
+                                    const mId = i + 1;
+                                    if(answer.select_employee.split(": ")[1] !== employeeList[i].first_name + " " + employeeList[i].last_name) {
+                                        managers.push(mId + ": " + employeeList[i].first_name + " " + employeeList[i].last_name);
+                                    };
+                                };
+                                
+                                managers.unshift("0: None");
+                                managers.unshift("E: Exit");
+                                return managers;
+                            },
+
+                            when: function( answers ) {
+                                return answers.select_role !== "0: Exit";
+                            }
+                        }
+                    ];
+
+                    inquirer.prompt(empPropPrompt)
+                    .then(function(answer) {
+                        if(answer.select_role == "0: Exit" || answer.select_manager == "E: Exit") {
+                            cli_prompt();
+                        }else{
+                            console.log(answer);
+                            let query = "UPDATE employees SET ? WHERE employees.id = " + empSelect;            
+                            connection.query(query,
+                            {
+                                role_id: parseInt(answer.select_role.split(":")[0]),            
+                                manager_id: parseInt(answer.select_manager.split(":")[0])
+                            },
+                            function(err, res){            
+                                if (err) throw err;
+                            });
+            
+                            let addagainPrompt = [
+                                {
+                            
+                                    name: "again",
+                                    type: "list",
+                                    message: "Would you like to add another employee?",
+                                    choices: ["Yes","Exit"]
+                                
+                                }
+                            ];
+            
+                            inquirer.prompt(addagainPrompt)            
+                            .then(function(answer) {            
+                                let query =
+
+                                    "SELECT employees.first_name, employees.last_name, roles.title, roles.salary, department.dept_name, employees.manager_id " +
+                                    "FROM employees " +
+                                    "JOIN roles ON roles.id = employees.role_id " +
+                                    "JOIN department ON roles.department_id = department.id " +
+                                    "ORDER BY employees.id;"
+
+                                ;
+
+                                connection.query(query, function(err,res) {                        
+                                    if (err) throw err;            
+                                    if(answer.again == "Yes") {            
+                                        updateEmployee();                                    
+                                    }else if(answer.again == "Exit") {                                        
+                                        for(i = 0; i < res.length; i++) {
+                                            if(res[i].manager_id == 0) {
+                                                res[i].manager = "None" 
+                                            }else{
+                                                res[i].manager = res[res[i].manager_id - 1].first_name + " " + res[res[i].manager_id - 1].last_name;
+                                            };
+
+                                            delete res[i].manager_id;
+                                        };
+
+                                        console.table(res);            
+                                        cli_prompt(); 
+                                    };  
+                                });
+                            }); 
+                        };
+                    });    
+                };
+            });
+        })
+    })
+};
